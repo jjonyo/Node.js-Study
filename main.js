e946f93408e1629e493c947aa78a7fa1e0f2eae7
jjonyo <jonghyuk96@cau.ac.kr>
2021-07-14 01:45:28 +0900

    프레임워크 없이 API만들기 (4)

--- main.js
+++ main.js
@@ -1,18 +1,58 @@
 // @ts-check
 const http = require('http')
 
+const posts = [
+  {
+    id: '1',
+    title: 'first',
+    content: 'first content',
+  },
+  {
+    id: '2',
+    title: 'second',
+    content: 'second content',
+  },
+  {
+    id: '3',
+    title: 'third',
+    content: 'third content',
+  },
+]
 const server = http.createServer((req, res) => {
   const POST_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_])+$/
   const regexResult = (req.url && POST_ID_REGEX.exec(req.url)) || undefined
 
   if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalLength: posts.length,
+    }
     res.statusCode = 200
-    res.end('[GET]posts')
-  } else if (regexResult) {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (regexResult && req.method === 'GET') {
     const postId = regexResult[1]
-    res.statusCode = 200
-    res.end(`[GET]posts/${postId}`)
+    const post = posts.find((_post) => _post.id === postId)
+    if (post) {
+      req.statusCode = 200
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Not found')
+    }
   } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      const reqBody = JSON.parse(data)
+      posts.push({
+        id: reqBody.title,
+        title: reqBody.title,
+        content: reqBody.content,
+      })
+    })
     res.statusCode = 200
     res.end('[POST]posts')
   } else {
